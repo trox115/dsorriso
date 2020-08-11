fb5539d6dcb1f89666442e37aaa711aa34a8c980
trox115 <tofernandes67@gmail.com>
2020-08-11 11:18:32 +0100

    navbar work

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
-	return <p>Hello</p>;
+	return <NavBar />;
 }
 
 export default App;
